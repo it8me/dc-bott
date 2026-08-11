a0e061078f4dd95731502ceefc98940f59cfa9b7
it4me <zenlozenka@gmail.com>
2026-08-11 14:53:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,10 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True  # важно для memberCount и событий входа/выхода
 bot = commands.Bot(command_prefix='.', intents=intents)
+
+WELCOME_CHANNEL_ID = 1536083742702043216
 
 @bot.event
 async def on_ready():
@@ -20,6 +23,22 @@ async def ping(ctx):
     rest = round((end - start) * 1000)
     gateway = round(bot.latency * 1000)
     
-    await msg.edit(content=f'pong! (gateway: {gateway}ms) (rest: {rest}ms)')
+    await msg.edit(content=f'Pong! (gateway: {gateway}ms) (rest: {rest}ms)')
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(WELCOME_CHANNEL_ID)
+    if channel:
+        await channel.send(
+            f'**к нам зашел новый участник! {member.mention}! получается, теперь нас {member.guild.member_count} *(включая ботов)*. ну шо сказать, классно!**'
+        )
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(WELCOME_CHANNEL_ID)
+    if channel:
+        await channel.send(
+            f'**у нас вышел участник {member}, оч жаль(( но получается, нас теперь {member.guild.member_count} *(включая ботов),* что есть то есть!**'
+        )
 
 bot.run(os.environ['TOKEN'])
