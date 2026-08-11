6dd0e3ca8886da4dc3152804cdf4b783a61ede88
it4me <zenlozenka@gmail.com>
2026-08-11 14:46:40 +0300

    Update bot command prefix and ping response

--- bot.py
+++ bot.py
@@ -4,7 +4,8 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='/', intents=intents)
+intents.message_content = True
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -13,12 +14,12 @@ async def on_ready():
 @bot.command(name='ping')
 async def ping(ctx):
     start = time.time()
-    msg = await ctx.send('Понг! 🏓')
+    msg = await ctx.send('Pong!')
     end = time.time()
     
-    latency = round((end - start) * 1000)
-    ws_latency = round(bot.latency * 1000)
+    rest = round((end - start) * 1000)
+    gateway = round(bot.latency * 1000)
     
-    await msg.edit(content=f'Понг! 🏓\nЗадержка сообщения: `{latency}ms`\nWebSocket: `{ws_latency}ms`')
+    await msg.edit(content=f'pong! (gateway: {gateway}ms) (rest: {rest}ms)')
 
 bot.run(os.environ['TOKEN'])
